c55a9b10b5a4da94c007c19446fbb77d61e74a52
turbinex-admin <ms@turbinex.io>
2022-03-13 23:20:16 -0500

    Upgrade to solidity v0.8.9.

--- contracts/ihelp/CharityPool.sol
+++ contracts/ihelp/CharityPool.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
--- contracts/ihelp/xHelpToken.sol
+++ contracts/ihelp/xHelpToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
