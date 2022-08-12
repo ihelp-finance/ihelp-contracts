5bd506f6318d4566d37129d88aa60ff7c3a50996
turbinex-admin <ms@turbinex.io>
2022-08-12 01:15:25 -0500

    Fixing test validations.

--- hardhat.config.js
+++ hardhat.config.js
@@ -39,7 +39,7 @@ let forkingData = undefined;
 if (process.env.TEST_FORK != '' && process.env.TEST_FORK != undefined) {
   forkingData = {
     url: process.env.TEST_FORK,
-    blockNumber: 18486761
+    // blockNumber: 18486761
   };
 }
 
