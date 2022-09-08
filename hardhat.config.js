4db0ea0db716fa6b285b217b189224908b73f37f
turbinex-admin <ms@turbinex.io>
2022-09-08 06:19:01 +0000

    Revision to hardhat tasks.

--- hardhat.config.js
+++ hardhat.config.js
@@ -184,7 +184,7 @@ module.exports = {
 
 };
 
-const DEBUG = true;
+const DEBUG = false;
 
 function debug(text) {
   if (DEBUG) {
@@ -259,36 +259,288 @@ task("fundedwallet", "Create a wallet (pk) link and fund it with deployer?")
 
 task(
   "generate",
-  "Create a mnemonic for builder deploys",
-  async (_, { ethers }) => {
-    const bip39 = require("bip39");
-    const hdkey = require("ethereumjs-wallet/hdkey");
-    const mnemonic = bip39.generateMnemonic();
-    if (DEBUG) console.log("mnemonic", mnemonic);
-    const seed = await bip39.mnemonicToSeed(mnemonic);
-    if (DEBUG) console.log("seed", seed);
-    const hdwallet = hdkey.fromMasterSeed(seed);
-    const wallet_hdpath = "m/44'/60'/0'/0/";
-    const account_index = 0;
-    let fullPath = wallet_hdpath + account_index;
-    if (DEBUG) console.log("fullPath", fullPath);
-    const wallet = hdwallet.derivePath(fullPath).getWallet();
-    const privateKey = "0x" + wallet._privKey.toString("hex");
-    if (DEBUG) console.log("privateKey", privateKey);
-    var EthUtil = require("ethereumjs-util");
-    const address =
-      "0x" + EthUtil.privateToAddress(wallet._privKey).toString("hex");
-    console.log(
-      "🔐 Account Generated as " +
-      address +
-      " and set as mnemonic in packages/hardhat"
-    );
-    console.log(
-      "💬 Use 'yarn run account' to get more information about the deployment account."
-    );
+  "Create a mnemonic for accounts")
+  .addOptionalParam("num", "Number of accounts to generate.")
+  .setAction(async (taskArgs, { network, ethers }) => {
 
-    fs.writeFileSync("./" + address + ".txt", mnemonic.toString());
-    fs.writeFileSync("./mnemonic.txt", mnemonic.toString());
+    const num = taskArgs.num ? parseInt(taskArgs.num) : 1;
+
+    for (let i =0; i < num; i++) {
+
+      const bip39 = require("bip39");
+      const hdkey = require("ethereumjs-wallet/hdkey");
+      const mnemonic = bip39.generateMnemonic();
+      if (DEBUG) console.log("mnemonic", mnemonic);
+      const seed = await bip39.mnemonicToSeed(mnemonic);
+      if (DEBUG) console.log("seed", seed);
+      const hdwallet = hdkey.fromMasterSeed(seed);
+      const wallet_hdpath = "m/44'/60'/0'/0/";
+      const account_index = 0;
+      let fullPath = wallet_hdpath + account_index;
+      if (DEBUG) console.log("fullPath", fullPath);
+      const wallet = hdwallet.derivePath(fullPath).getWallet();
+      const privateKey = "0x" + wallet._privKey.toString("hex");
+      if (DEBUG) console.log("privateKey", privateKey);
+      var EthUtil = require("ethereumjs-util");
+      const address =
+        "0x" + EthUtil.privateToAddress(wallet._privKey).toString("hex");
+      console.log(i+1,'/',num,address);
+
+      const walletDir = './wallets';
+      if (!fs.existsSync(walletDir)){
+          fs.mkdirSync(walletDir, { recursive: true });
+      }
+  
+      fs.writeFileSync(walletDir + "/" + address + ".txt", privateKey.toString());
+
+    }
+
+  }
+);
+
+task(
+  "transferToWallets",
+  "Transfer specific amount to wallet accounts.")
+  .addOptionalParam("min", "Min amount to transfer")
+  .addOptionalParam("max", "Min amount to transfer")
+  .setAction(async (taskArgs, { getNamedAccounts,network, ethers,getChainId, deployments }) => {
+
+      const hdkey = require("ethereumjs-wallet/hdkey");
+      const bip39 = require("bip39");
+
+      const chainId = await getChainId();
+
+      let {
+        deployer
+      } = await getNamedAccounts();
+    
+      console.log('\n',deployer)
+      signer = await ethers.provider.getSigner(deployer);
+
+      const balance = await ethers.provider.getBalance(signer._address);
+      console.log("   avax balance: " + ethers.utils.formatEther(balance));
+
+      if (taskArgs.min != undefined && taskArgs.max != undefined) {
+
+        const accounts = [];
+        fs.readdirSync("wallets", "utf8").forEach((file) => {
+          // accounts.push(fs.readFileSync("wallets/" + file, "utf8"));
+          accounts.push(file.replace('.txt',''));
+        });
+
+        const native = '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7';
+        const currency = '0xd586E7F844cEa2F87f50152665BCbc2C279D8d70';
+
+        // swap avax to wavax
+        const external_contracts = require('../ihelp-app/client/src/contracts/external_contracts');
+        const wavaxabi= external_contracts[chainId]['contracts']['WAVAX']['abi'];
+        
+        const wrappedNative = new ethers.Contract(native, wavaxabi, signer);
+        const dai = await ethers.getContractAt("ERC20", currency);
+
+        const daibalance = await dai.balanceOf(signer._address);
+        let wavaxbalance = await wrappedNative.balanceOf(signer._address);
+
+        // use swapper to convert avax to dai
+        const swapperAddress = (await deployments.get('swapper')).address;
+        swapper = await ethers.getContractAt('Swapper', swapperAddress, signer);
+
+        const WRAP_AVAX = false;
+        const SWAP_TO_DAI = false;
+        const DO_TRANSFERS = false;
+        const DO_DONATIONS = false;
+
+        if (WRAP_AVAX) {
+          const nativeAmount = await swapper.getNativeRoutedTokenPrice(currency, native, ethers.utils.parseUnits('180',18));
+          const wrapTx = await wrappedNative.deposit({value: nativeAmount });
+          await wrapTx.wait();
+        }    
+
+        if (SWAP_TO_DAI) {
+
+          wavaxbalance = await wrappedNative.balanceOf(signer._address);
+          
+          const currencyAmount = await swapper.getNativeRoutedTokenPrice(native, currency, wavaxbalance);
+          const minamount = currencyAmount.mul(95).div(100);
+
+          const approveTx = await wrappedNative.approve(swapperAddress, wavaxbalance);
+          await approveTx.wait();
+
+          const swapTx = await swapper.swap(native,currency,wavaxbalance.toString(), minamount, signer._address)
+          await swapTx.wait();
+
+        }
+
+        console.log("   wavax balance: " + ethers.utils.formatEther(wavaxbalance));
+        console.log("   dai balance: " + ethers.utils.formatEther(daibalance));
+
+        console.log('\n',accounts.length,'accounts found...\n');
+
+        let totalTransfered = 0;
+        let totalDonated = 0;
+
+        let deployedCharities = [];
+        if (DO_DONATIONS) {
+          const FILE_DIR = 'build'
+          const FILE_PATH = path.join(FILE_DIR, `${defaultNetwork}_charities.json`);
+          if (fs.existsSync(FILE_PATH)) {
+            const fileData = fs.readFileSync(FILE_PATH, { encoding: 'utf-8' });
+            deployedCharities = JSON.parse(fileData);
+          }
+        }
+
+        for (let a in accounts) {
+
+          let userbalance = await ethers.provider.getBalance(accounts[a]);
+          let daibalance = await dai.balanceOf(accounts[a]);
+          console.log(parseInt(a)+1,'/',accounts.length,accounts[a],'avax:'+ethers.utils.formatEther(userbalance),'dai:'+ethers.utils.formatEther(daibalance));
+
+          if (DO_TRANSFERS) {
+            const amountToTransfer = Math.random() * (parseFloat(taskArgs.max) - parseFloat(taskArgs.min)) + parseFloat(taskArgs.min);
+            console.log('   transferring',amountToTransfer);
+            const transferTx = await dai.transfer(accounts[a],ethers.utils.parseUnits(amountToTransfer.toString(),18));
+            await transferTx.wait();
+            daibalance = await dai.balanceOf(accounts[a]);
+            console.log('   newbalance',ethers.utils.formatEther(daibalance));
+            totalTransfered+=amountToTransfer;
+          }
+
+          if (DO_DONATIONS && daibalance > 0) {
+
+            const rndInt = Math.floor(Math.random() * deployedCharities.length);
+            const charity = deployedCharities[rndInt];
+
+            console.log('   donating',ethers.utils.formatEther(daibalance),'to',charity.charityName);
+
+            const walletFile = `./wallets/${accounts[a]}.txt`;
+
+            let privKey = fs.readFileSync(walletFile).toString().trim();
+            const userSigner = new ethers.Wallet(privKey, ethers.provider);
+
+            if (userbalance == 0) {
+
+              const txAmount = 0.02;
+
+              console.log('   transfering',txAmount,'avax to user...');
+              const txRequest = {
+                from: signer._address,
+                to: userSigner.address,
+                value: ethers.utils.parseUnits(txAmount.toString(), "ether").toHexString(),
+                chainId: chainId,
+              };
+
+              const transferTx = await send(signer, txRequest);
+              await transferTx.wait()
+
+              userbalance = await ethers.provider.getBalance(userSigner.address);
+              console.log("   new user avax balance: " + ethers.utils.formatEther(userbalance));
+
+            }
+
+            const approveTx = await dai.connect(userSigner).approve(charity.address, daibalance.toString());
+            await approveTx.wait();
+
+            const charityInstance = await ethers.getContractAt("CharityPool", charity.address);
+
+            const lender = '0x82E64f49Ed5EC1bC6e43DAD4FC8Af9bb3A2312EE';
+            const donateTx = await charityInstance.connect(userSigner).depositTokens(lender,daibalance.toString(),"");
+            await donateTx.wait();
+
+            const donationbalance = await charityInstance.balanceOf(userSigner.address,lender);
+            console.log('   donationbalance',parseFloat(ethers.utils.formatEther(donationbalance)));
+            
+            userbalance = await ethers.provider.getBalance(userSigner.address);
+            console.log("   user avax balance: " + ethers.utils.formatEther(userbalance));
+
+            totalDonated+=parseFloat(ethers.utils.formatEther(donationbalance));
+
+            // process.exit(0)
+
+          }
+
+        }
+        
+        if (DO_TRANSFERS) {
+          console.log('\ntotalTransfered',totalTransfered);
+        }
+        if (DO_DONATIONS) {
+          console.log('\ntotalDonated',totalDonated);
+        }
+
+      }
+
+  })
+
+
+task(
+  "account",
+  "Get balance informations for the deployment account.")
+  .addOptionalParam("address", "Address to lookup")
+  .setAction(async (taskArgs, { getNamedAccounts,network, ethers }) => {
+
+    let walletFile = taskArgs.address ? './wallets/' + taskArgs.address + '.txt' : false;
+
+    if (walletFile == false) {
+
+      let {
+        deployer
+      } = await getNamedAccounts();
+    
+      console.log(deployer)
+      signer = await ethers.provider.getSigner(deployer);
+
+      const balance = await ethers.provider.getBalance(signer._address);
+      console.log("   balance: " + ethers.utils.formatEther(balance));
+
+
+    } else {
+
+      const hdkey = require("ethereumjs-wallet/hdkey");
+      const bip39 = require("bip39");
+      let mnemonic = fs.readFileSync(walletFile).toString().trim();
+      if (DEBUG) console.log("mnemonic", mnemonic);
+      const seed = await bip39.mnemonicToSeed(mnemonic);
+      if (DEBUG) console.log("seed", seed);
+      const hdwallet = hdkey.fromMasterSeed(seed);
+      const wallet_hdpath = "m/44'/60'/0'/0/";
+      const account_index = 0;
+      let fullPath = wallet_hdpath + account_index;
+      if (DEBUG) console.log("fullPath", fullPath);
+      const wallet = hdwallet.derivePath(fullPath).getWallet();
+      const privateKey = "0x" + wallet._privKey.toString("hex");
+      if (DEBUG) console.log("privateKey", privateKey);
+      var EthUtil = require("ethereumjs-util");
+      const address =
+        "0x" + EthUtil.privateToAddress(wallet._privKey).toString("hex");
+
+      var qrcode = require("qrcode-terminal");
+      qrcode.generate(address);
+
+      const networks = {} // config.networks
+      networks[defaultNetwork] = config.networks[defaultNetwork]
+
+      for (let n in networks) {
+        //console.log(config.networks[n],n)
+        try {
+          let provider = new ethers.providers.JsonRpcProvider(
+            config.networks[n].url
+          );
+          let balance = await provider.getBalance(address);
+          //console.log(" -- " + n + " -- ");
+          console.log("   balance: " + ethers.utils.formatEther(balance));
+          // console.log(
+          //   "   nonce: " + (await provider.getTransactionCount(address))
+          // );
+        }
+        catch (e) {
+          if (DEBUG) {
+            console.log(e);
+          }
+        }
+      }
+
+    }
+
   }
 );
 
@@ -358,53 +610,6 @@ task(
     fs.writeFileSync("./mnemonic.txt", mnemonic.toString());
   });
 
-task(
-  "account",
-  "Get balance informations for the deployment account.",
-  async (_, { ethers }) => {
-    const hdkey = require("ethereumjs-wallet/hdkey");
-    const bip39 = require("bip39");
-    let mnemonic = fs.readFileSync("./mnemonic.txt").toString().trim();
-    if (DEBUG) console.log("mnemonic", mnemonic);
-    const seed = await bip39.mnemonicToSeed(mnemonic);
-    if (DEBUG) console.log("seed", seed);
-    const hdwallet = hdkey.fromMasterSeed(seed);
-    const wallet_hdpath = "m/44'/60'/0'/0/";
-    const account_index = 0;
-    let fullPath = wallet_hdpath + account_index;
-    if (DEBUG) console.log("fullPath", fullPath);
-    const wallet = hdwallet.derivePath(fullPath).getWallet();
-    const privateKey = "0x" + wallet._privKey.toString("hex");
-    if (DEBUG) console.log("privateKey", privateKey);
-    var EthUtil = require("ethereumjs-util");
-    const address =
-      "0x" + EthUtil.privateToAddress(wallet._privKey).toString("hex");
-
-    var qrcode = require("qrcode-terminal");
-    qrcode.generate(address);
-    console.log("‍📬 Deployer Account is " + address);
-    for (let n in config.networks) {
-      //console.log(config.networks[n],n)
-      try {
-        let provider = new ethers.providers.JsonRpcProvider(
-          config.networks[n].url
-        );
-        let balance = await provider.getBalance(address);
-        console.log(" -- " + n + " --  -- -- 📡 ");
-        console.log("   balance: " + ethers.utils.formatEther(balance));
-        console.log(
-          "   nonce: " + (await provider.getTransactionCount(address))
-        );
-      }
-      catch (e) {
-        if (DEBUG) {
-          console.log(e);
-        }
-      }
-    }
-  }
-);
-
 async function addr(ethers, addr) {
   if (isAddress(addr)) {
     return getAddress(addr);
@@ -417,8 +622,22 @@ async function addr(ethers, addr) {
 }
 
 task("accounts", "Prints the list of accounts", async (_, { ethers }) => {
-  const accounts = await ethers.provider.listAccounts();
-  accounts.forEach((account) => console.log(account));
+  // const accounts = await ethers.provider.listAccounts();
+  // accounts.forEach((account) => console.log(account));
+
+  let provider = new ethers.providers.JsonRpcProvider(config.networks[defaultNetwork].url);
+
+  const accounts = [];
+  fs.readdirSync("wallets", "utf8").forEach((file) => {
+    // accounts.push(fs.readFileSync("wallets/" + file, "utf8"));
+    accounts.push(file.replace('.txt',''));
+  });
+  console.log(accounts.length,'accounts found...');
+  for (let a in accounts) {
+    let balance = await provider.getBalance(accounts[a]);
+    console.log(accounts[a]+' '+ethers.utils.formatEther(balance));
+  }
+
 });
 
 task("blockNumber", "Prints the block number", async (_, { ethers }) => {
