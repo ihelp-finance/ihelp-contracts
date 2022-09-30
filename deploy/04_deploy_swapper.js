4facf20f39eda757426a62be50d403cef4290ffa
turbinex-admin <ms@turbinex.io>
2022-09-30 22:56:39 +0000

    Fixing default swapper selection

--- deploy/04_deploy_swapper.js
+++ deploy/04_deploy_swapper.js
@@ -25,7 +25,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   
   
   let nativeTokenAddress = null;
-  if (process.env.SWAPPER_ADDRESSES == 'traderjoe') {
+  if ( (process.env.SWAPPER_ADDRESSES || 'traderjoe') == 'traderjoe') {
     
     const abi = [
       {
